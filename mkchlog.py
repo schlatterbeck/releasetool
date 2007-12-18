ea8829bd81a39dcc8ca4f9cf7720faa9cc54a3df
Ralf Schlatterbeck <rsc@runtux.com>
2007-12-18 17:48:25 +0000

    TeX and SF make
    Add txt to html conversion to tex makefile
    First version of SF stuff

---
+++ mkchlog.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+import re
+
+
+state = 'start'
+for line in sys.stdin :
+    line = line.rstrip ()
+    if line == 'Changes' :
+        state = 'changelog'
+    if line.startswith ('Version') and state == 'changelog' :
+        state = 'start_notes'
+        continue
+    if not line and state == 'notes' :
+        state = 'start_changes'
+        continue
+    if line and state.startswith ('start_') :
+        state = state [6:]
+    if sys.argv [1] == state :
+        print line
