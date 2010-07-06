6fa5e8ce9bb131f99429b178f81eb65d21551b55
Ralf Schlatterbeck <rsc@runtux.com>
2010-07-06 14:23:59 +0000

    First stab at freshmeat announcer

---
+++ freshmeat.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+from rsclib.Freshmeat import Freshmeat, Release
+from optparse         import OptionParser
+
+usage  = "%proc [options] project changenote-file version"
+parser = OptionParser (usage = usage)
+parser.add_option \
+    ( "-t", "--tag"
+    , dest    = "tag"
+    , action  = "append"
+    , help    = "Tag for release (may be specified more than once)"
+    , default = []
+    )
+(opt, args) = parser.parse_args ()
+if len (args) != 3 :
+    parser.error ("Need three arguments")
+    sys.exit (23)
+
+changelog = open (args [1], 'r').read ()
+r  = Release   (args [2], changelog, False, *opt.tag)
+fm = Freshmeat (args [0], 'releases', put = r.element)
